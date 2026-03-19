969167fc9e19028a68c5f4714dbe98b3a4804cdb
charles <1126104236@qq.com>
2026-03-19 19:24:44 +0800

    fix: complete config path migration to ~/.config/clashfx/
    - StatusItemTool.swift: menuImage path
    - ProxyConfig/main.m: proxyIgnoreList path
    - sampleConfig.yaml: user-facing path reference
    - ClashResourceManager + all Localizable.strings: alert message paths

--- ClashFX/Views/StatusItem/StatusItemTool.swift
+++ ClashFX/Views/StatusItem/StatusItemTool.swift
@@ -10,7 +10,7 @@ import AppKit
 
 enum StatusItemTool {
     static let menuImage: NSImage = {
-        let customImagePath = (NSHomeDirectory() as NSString).appendingPathComponent("/.config/clash/menuImage.png")
+        let customImagePath = (NSHomeDirectory() as NSString).appendingPathComponent("/.config/clashfx/menuImage.png")
         if let image = NSImage(contentsOfFile: customImagePath) {
             image.isTemplate = true
             return image
